1ff538ee4bb2f6d448eaf26371a90ae92fc19fe3
wrqqq <wrqqqrrr@gmail.com>
2017-03-10 13:37:45 +0300

    Second section, adaptivity

--- app/js/common.js
+++ app/js/common.js
@@ -24,5 +24,50 @@ $(function() {
 		});
 		return false;
 	});
+var theToggle = document.getElementById('toggle');
+var menu = document.querySelector('nav');
 
+// based on Todd Motto functions
+// http://toddmotto.com/labs/reusable-js/
+
+// hasClass
+function hasClass(elem, className) {
+	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+// addClass
+function addClass(elem, className) {
+    if (!hasClass(elem, className)) {
+    	elem.className += ' ' + className;
+    }
+}
+// removeClass
+function removeClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+	if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+}
+// toggleClass
+function toggleClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+            newClass = newClass.replace( " " + className + " " , " " );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
+	$(theToggle).click(function() {
+		$(".nav").slideToggle();
+		return false;
+	});
+theToggle.onclick = function() {
+   toggleClass(this, 'on');
+   return false;
+}	
 });
